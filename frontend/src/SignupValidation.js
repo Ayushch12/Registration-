736dd334eab556b9f5538ca7db883162133d3bcc
Ayush chalisee <ayushchalise12@gmail.com>
2023-05-18 15:11:00 +0200

    Adding LoginValidation

---
+++ frontend/src/SignupValidation.js
@@ -0,0 +1,24 @@
+function validation(values) {
+    let  error = {}
+    const email_Pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const password_Pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/;
+
+    if (values.email === "") {
+      error.email = "Name must not be empty";
+    } else if (!email_Pattern.test(values.email)) {
+      error.email = "Email does not match";
+    } else {
+        error.email = "";
+    }
+
+    if (values.password === "") {
+      error.password =  "Password must not be empty" ;
+    } else if (!password_Pattern.test(values.password)) {
+      error.password = " Password does not match ";
+    } else {
+        error.password = "";
+    }
+    return error;
+  }
+
+  export default validation;
